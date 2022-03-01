746976503f86d8f138f01ddba3879151b1c03f11
yuriy kormin <yuriy.kormin@gmail.com>
2022-03-01 14:15:13 +0300

    change 'if' checking to assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,5 @@
 from project_capitalize.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
 print('Все тесты пройдены!')
